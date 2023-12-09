562363137a7a837b8bcf73b918d1b44144831cf6
SOURAV MEHTA <mehtas018@gmail.com>
2023-12-09 21:25:26 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = 33
+
+console.log(typeof score)
+console.log(typeof (score))
+
+console.log("----------")
+
+let a= "33"
+let b= Number(a)
+console.log(typeof a)
+console.log(typeof b)
+
+console.log("----------")
+
+let c="33xyz"
+let d= Number(c)
+console.log(typeof c)
+console.log(typeof d)
+console.log(d)
+
+console.log("----------")
+
+let e= undefined
+let f= true
+let g = Number(f)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
+console.log(e)
+console.log(f)
+console.log(g)
+
+console.log("----------")
+
+let isLoggedIn = 1
+let bool = Boolean(isLoggedIn)
+console.log(bool)
