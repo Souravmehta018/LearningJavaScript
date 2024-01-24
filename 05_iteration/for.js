cfeb4ac1e23bf319132e0d1ac48fa5be2e99f185
SOURAV MEHTA <mehtas018@gmail.com>
2024-01-24 19:26:01 +0000

    while

---
+++ 05_iteration/for.js
@@ -0,0 +1,25 @@
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        console.log(`outer loop ${i} & loop `+j);
+    }
+    console.log(`outer loop ${i}`);
+}
+
+// break
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        console.log(`won't print: ${i}`);
+        break;
+    }
+    console.log(`break: ${i}`);
+}
+// continue
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        console.log(`won't print: ${i}`);
+        continue;
+    }
+    console.log(`continue: ${i}`);
+}
---
+++ 05_iteration/while.js
@@ -0,0 +1,7 @@
+// while loop
+let i=0
+let a = ["abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz"];
+while (i < a.length) {
+    console.log(a[i]);
+    i++;
+}
