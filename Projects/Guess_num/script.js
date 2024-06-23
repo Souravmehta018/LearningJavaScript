69c1819c20f6a9fc0b452972ec50dfbb1bffe903
SOURAV MEHTA <mehtas018@gmail.com>
2024-06-23 12:55:40 +0000

    geuss

---
+++ Projects/Guess_num/script.js
@@ -0,0 +1,60 @@
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+const submit = document.getElementById('subt');
+const userInput = document.getElementById('guessField');
+const guesses = document.getElementsByClassName('guesses');
+const chancesLeft = document.getElementsByClassName('chancesLeft');
+
+let gs = [];
+let gscount = 10;
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    if (validateGuess(guess) == true){
+        if (guess === random) {
+            alert('Congratulations, you guessed the number!');
+            alert('The game will reset in 5 seconds');
+            setInterval(function() {
+                window.location.reload();
+            }, 5000);
+        } 
+        else {
+            //console.log(guess);
+            gs.push(guess);
+            console.log(gs);
+            guesses.innerHTML = gs;
+            
+            if (guess < random) {
+            alert('Too low!');        
+            } 
+            else {
+            alert('Too high!');
+            }
+        }
+    gscount--;
+    console.log(gscount);
+    //guesses.innerHTML = gscount;
+    }
+    
+});
+
+function validateGuess(guess) {
+    if (guess < 1 || guess > 100 || isNaN(guess)) {
+        alert('Please enter a valid number between 1 and 100');
+        return false;
+    }
+    else if (gscount === 0) {
+        alert('You have reached the maximum number of guesses');
+        alert('The game will reset in 5 seconds');
+        setInterval(function() {
+            window.location.reload();
+        }, 5000);
+        return false;
+    }
+    else if (gs.includes(guess)) {
+        alert('You have already guessed that number');
+        return false;
+    }
+    else return true;
+}
